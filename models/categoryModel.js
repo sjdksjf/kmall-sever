dd86632237b55360992ab7e806deaa998a7517d1
MK <929293846@qq.com>
2018-09-28 13:02:19 +0800

    add

---
+++ models/categoryModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const pagination = require('../util/pagination.js');
+const CategorySchema = new mongoose.Schema({
+        name:{
+            type:String,
+        },
+        order:{
+        type:Number,
+        default:0
+        },
+        pid:{
+            type:String
+        }
+    },{timestamps:true});
+
+CategorySchema.statics.getPaginationCategories = function(page,query={}){
+    return new Promise((resolve,reject)=>{
+      let options = {
+        page: page,//需要显示的页码
+        model:this, //操作的数据模型
+        query:query, //查询条件
+        projection:'id name order pid', //投影，
+        sort:{order:-1}, //排序
+      }
+      pagination(options)
+      .then((data)=>{
+        resolve(data); 
+      })
+    })
+ }
+let categoryModel = mongoose.model('Category',CategorySchema);
+module.exports=categoryModel;
---
+++ models/resource.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+
+const ResourceSchema = new mongoose.Schema({
+  name:{
+  	type:String
+  },
+  path:{
+  	type:String
+  }
+});
+
+
+const ResourceModel = mongoose.model('Resource', ResourceSchema);
+
+module.exports = ResourceModel;
